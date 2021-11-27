b829119063aa77ecc5d7e8b072500159a7eb4f1f
Takács Balázs <tbalazs.levente@gmail.com>
2021-11-27 12:56:22 +0100

    Adatbázisba adat felvétel, listázás

--- app/src/main/java/com/example/plinkaapp/DBHelper.java
+++ app/src/main/java/com/example/plinkaapp/DBHelper.java
@@ -1,6 +1,8 @@
 package com.example.plinkaapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -36,5 +38,20 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean rogzites(String fozo, String gyumolcs, int alkohol) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COL_FOZO, fozo);
+        values.put(COL_GYUMOLCS, gyumolcs);
+        values.put(COL_ALKOHOL, alkohol);
+        return db.insert(TABLE_NAME, null, values) != -1;
+    }
+
+    public Cursor listazas() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.query(TABLE_NAME, new String[]{COL_ID, COL_FOZO, COL_GYUMOLCS, COL_ALKOHOL},
+                null, null, null, null, null);
     }
 }
