d5e883daa6e1026584f327917da1cd01c5d3153a
Takács Balázs <tbalazs.levente@gmail.com>
2021-11-27 13:30:20 +0100

    Felvtel activity kinézet, inicializálás

--- app/src/main/java/com/example/plinkaapp/AdatFelvetelActivity.java
+++ app/src/main/java/com/example/plinkaapp/AdatFelvetelActivity.java
@@ -3,12 +3,26 @@ package com.example.plinkaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AdatFelvetelActivity extends AppCompatActivity {
+    private EditText editTxt_fozo, editTxt_gyumolcs, editTxt_alkoholtartalom;
+    private Button btnFelvetel_felvetel, btnFelvetel_vissza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adat_felvetel);
+
+        init();
+    }
+
+    private void init() {
+        editTxt_fozo = findViewById(R.id.editTxt_fozo);
+        editTxt_gyumolcs = findViewById(R.id.editTxt_gyumolcs);
+        editTxt_alkoholtartalom = findViewById(R.id.editTxt_alkoholtartalom);
+        btnFelvetel_felvetel = findViewById(R.id.btnFelvetel_felvetel);
+        btnFelvetel_vissza = findViewById(R.id.btnFelvetel_vissza);
     }
 }
--- app/src/main/java/com/example/plinkaapp/MainActivity.java
+++ app/src/main/java/com/example/plinkaapp/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.plinkaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn_felvetel, btn_kereses, btn_listazas;
     private TextView txtView_lista;
+    private DBHelper adatbazis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +42,30 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        btn_listazas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor adatok = adatbazis.listazas();
+                if (adatok.getCount() == 0) {
+                    Toast.makeText(MainActivity.this, "Nincs adat az adatbázisban", Toast.LENGTH_SHORT).show();
+                } else {
+                    StringBuilder sb = new StringBuilder();
+                    while (adatok.moveToNext()) {
+                        sb.append("ID: ").append(adatok.getInt(0));
+                        sb.append(System.lineSeparator());
+                        sb.append("Főző: ").append(adatok.getString(1));
+                        sb.append(System.lineSeparator());
+                        sb.append("Gyümölcs: ").append(adatok.getString(2));
+                        sb.append(System.lineSeparator());
+                        sb.append("Alkoholtartalom: ").append(adatok.getInt(3));
+                        sb.append(System.lineSeparator());
+                        sb.append(System.lineSeparator());
+                    }
+                    txtView_lista.setText(sb.toString());
+                }
+            }
+        });
     }
 
     private void init() {
@@ -46,5 +73,6 @@ public class MainActivity extends AppCompatActivity {
         btn_kereses = findViewById(R.id.btn_kereses);
         btn_listazas = findViewById(R.id.btn_listazas);
         txtView_lista = findViewById(R.id.txtView_lista);
+        adatbazis = new DBHelper(this);
     }
 }
